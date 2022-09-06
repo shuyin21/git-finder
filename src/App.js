757aa77327b03d47c7d102ca5e4ac8f7ec705e79
shuyin21 <shuyindev@gmail.com>
2022-09-06 16:07:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className='coontainer mx-auto px-3 pb-12'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/notfound' element={<NotFound />} />
+            <Route exact path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+
+
+    </Router>
+
+
+
   );
 }
 
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+
+    const footerYear = new Date().getFullYear()
+
+    return (
+        <footer className='footer p-10 bg-gray-700 text-primary-content footer-center'>
+            <p>Copyright {footerYear} All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
